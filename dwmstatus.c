355de46aa0e577e2539a554db4ad7e4cf658b286
William Giokas <1007380@gmail.com>
2012-12-25 01:32:48 -0600

    Added network and battery stuff
    Signed-off-by: William Giokas <1007380@gmail.com>

--- dwmstatus.c
+++ dwmstatus.c
@@ -12,9 +12,10 @@
 
 #include <X11/Xlib.h>
 
-char *tzargentina = "America/Buenos_Aires";
+/*char *tzargentina = "America/Buenos_Aires";
 char *tzutc = "UTC";
-char *tzberlin = "Europe/Berlin";
+char *tzberlin = "Europe/Berlin";*/
+char *tzchicago = "America/Chicago";
 
 static Display *dpy;
 
@@ -92,14 +93,170 @@ loadavg(void)
 	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
 }
 
+/* BATTERY USAGE
+ */
+char *
+readfile(char *base, char *file)
+{
+	char *path, line[513];
+	FILE *fd;
+
+	memset(line, 0, sizeof(line));
+
+	path = smprintf("%s/%s", base, file);
+	fd = fopen(path, "r");
+	if (fd == NULL)
+		return NULL;
+	free(path);
+
+	if (fgets(line, sizeof(line)-1, fd) == NULL)
+		return NULL;
+	fclose(fd);
+
+	return smprintf("%s", line);
+}
+
+/*
+ * Linux seems to change the filenames after suspend/hibernate
+ * according to a random scheme. So just check for both possibilities.
+ */
+char *
+getbattery(char *base)
+{
+	char *co;
+	int descap, remcap;
+
+	descap = -1;
+	remcap = -1;
+
+	co = readfile(base, "present");
+	if (co == NULL || co[0] != '1') {
+		if (co != NULL) free(co);
+		return smprintf("not present");
+	}
+	free(co);
+
+	co = readfile(base, "charge_full_design");
+	if (co == NULL) {
+		co = readfile(base, "energy_full_design");
+		if (co == NULL)
+			return smprintf("");
+	}
+	sscanf(co, "%d", &descap);
+	free(co);
+
+	co = readfile(base, "charge_now");
+	if (co == NULL) {
+		co = readfile(base, "energy_now");
+		if (co == NULL)
+			return smprintf("");
+	}
+	sscanf(co, "%d", &remcap);
+	free(co);
+
+	if (remcap < 0 || descap < 0)
+		return smprintf("invalid");
+
+	return smprintf("%.0f", ((float)remcap / (float)descap) * 100);
+}
+/* END BATTERY USAGE
+ */
+
+/* NETWORK STUFF
+ */
+parse_netdev(unsigned long long int *receivedabs, unsigned long long int *sentabs)
+{
+	char *buf;
+	char *eth0start;
+	static int bufsize;
+	FILE *devfd;
+
+	buf = (char *) calloc(255, 1);
+	bufsize = 255;
+	devfd = fopen("/proc/net/dev", "r");
+
+	// ignore the first two lines of the file
+	fgets(buf, bufsize, devfd);
+	fgets(buf, bufsize, devfd);
+
+	while (fgets(buf, bufsize, devfd)) {
+	    if ((eth0start = strstr(buf, "eth0:")) != NULL) {
+
+		// With thanks to the conky project at http://conky.sourceforge.net/
+		sscanf(eth0start + 6, "%llu  %*d     %*d  %*d  %*d  %*d   %*d        %*d       %llu",\
+		       receivedabs, sentabs);
+		fclose(devfd);
+		free(buf);
+		return 0;
+	    }
+	}
+	fclose(devfd);
+	free(buf);
+	return 1;
+}
+
+char *
+get_netusage()
+{
+	unsigned long long int oldrec, oldsent, newrec, newsent;
+	double downspeed, upspeed;
+	char *downspeedstr, *upspeedstr;
+	char *retstr;
+	int retval;
+
+	downspeedstr = (char *) malloc(15);
+	upspeedstr = (char *) malloc(15);
+	retstr = (char *) malloc(42);
+
+	retval = parse_netdev(&oldrec, &oldsent);
+	if (retval) {
+	    fprintf(stdout, "Error when parsing /proc/net/dev file.\n");
+	    exit(1);
+	}
+
+	sleep(1);
+	retval = parse_netdev(&newrec, &newsent);
+	if (retval) {
+	    fprintf(stdout, "Error when parsing /proc/net/dev file.\n");
+	    exit(1);
+	}
+
+	downspeed = (newrec - oldrec) / 1024.0;
+	if (downspeed > 1024.0) {
+	    downspeed /= 1024.0;
+	    sprintf(downspeedstr, "%.3f MB/s", downspeed);
+	} else {
+	    sprintf(downspeedstr, "%.2f KB/s", downspeed);
+	}
+
+	upspeed = (newsent - oldsent) / 1024.0;
+	if (upspeed > 1024.0) {
+	    upspeed /= 1024.0;
+	    sprintf(upspeedstr, "%.3f MB/s", upspeed);
+	} else {
+	    sprintf(upspeedstr, "%.2f KB/s", upspeed);
+	}
+	sprintf(retstr, "down: %s up: %s", downspeedstr, upspeedstr);
+
+	free(downspeedstr);
+	free(upspeedstr);
+	return retstr;
+}
+
+/* END NETWORK STUFF
+ */
+
 int
 main(void)
 {
 	char *status;
 	char *avgs;
-	char *tmar;
+	/*char *tmar;
 	char *tmutc;
-	char *tmbln;
+	char *tmbln;*/
+    char *tmchi;
+    char *batt;
+    char *net;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
@@ -108,17 +265,23 @@ main(void)
 
 	for (;;sleep(90)) {
 		avgs = loadavg();
-		tmar = mktimes("%H:%M", tzargentina);
+		/*tmar = mktimes("%H:%M", tzargentina);
 		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
+		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);*/
+		tmchi = mktimes("%H:%M", tzchicago);
+        batt  = getbattery("/sys/class/power_supply/BAT0");
+        net   = get_netusage();
 
-		status = smprintf("L:%s A:%s U:%s %s",
-				avgs, tmar, tmutc, tmbln);
+		status = smprintf("Network: %s | Batt: %s | Load:%s | %s",
+				net, batt, avgs, /*tmar, tmutc, tmbln*/ tmchi);
 		setstatus(status);
 		free(avgs);
-		free(tmar);
+		/*free(tmar);
 		free(tmutc);
-		free(tmbln);
+		free(tmbln);*/
+        free(tmchi);
+        free(batt);
+        free(net);
 		free(status);
 	}
 
